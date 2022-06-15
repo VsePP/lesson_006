97fab415895b7b43315e93bbad85b0c2e7e72d59
Denis <vsepp@yandex.ru>
2022-06-15 08:45:25 +0500

    alpha version of game ver. 00000000.1

--- python_snippets/04_practice.py
+++ python_snippets/04_practice.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
-
-
+from nim_engine import put_stones, get_bunches, is_gameover, take_from_bunch
+from termcolor import cprint, colored
+put_stones()
+user_number = 1
+while True:
+    cprint('Текущая позиция', color='green')
+    cprint(get_bunches(), color='green')
+    user_color = 'blue' if user_number==1 else 'yellow'
+    cprint('Ход игрока {}'.format(user_number), color=user_color)
+    pos = input(colored('Откуда берем? ', color=user_color))
+    qua = input(colored('Сколько берём? ', color=user_color))
+    take_from_bunch(int(pos), int(qua))
+    if is_gameover():
+        break
+    user_number = 2 if user_number==1 else 1
+cprint('Выйграл игрок с номером ' + str(user_number), color='red')
 # Ним — математическая игра, в которой два игрока по очереди берут предметы,
 # разложенные на несколько кучек. За один ход может быть взято любое количество предметов
 # (большее нуля) из одной кучки. Выигрывает игрок, взявший последний предмет.
@@ -22,24 +36,24 @@
 #  ввод вторым игроком хода - позицию и кол-во камней
 #  вывод расположения камней
 
-from nim_engine import put_stones, get_bunches, is_gameover, take_from_bunch
-from termcolor import cprint, colored
-
-put_stones()
-user_number = 1
-while True:
-    cprint('Текущая позиция', color='green')
-    cprint(get_bunches(), color='green')
-    user_color = 'blue' if user_number == 1 else 'yellow'
-    cprint('Ход игрока {}'.format(user_number), color=user_color)
-    pos = input(colored('Откуда берем?', color=user_color))
-    qua = input(colored('Сколько берем?', color=user_color))
-    step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
-    if step_successed:
-        user_number = 2 if user_number == 1 else 1
-    else:
-        cprint('Невозможный ход!', color='red')
-    if is_gameover():
-        break
-
-cprint('Выйграл игрок номер {}'.format(user_number), color='red')
+# from nim_engine import put_stones, get_bunches, is_gameover, take_from_bunch
+# from termcolor import cprint, colored
+#
+# put_stones()
+# user_number = 1
+# while True:
+#     cprint('Текущая позиция', color='green')
+#     cprint(get_bunches(), color='green')
+#     user_color = 'blue' if user_number == 1 else 'yellow'
+#     cprint('Ход игрока {}'.format(user_number), color=user_color)
+#     pos = input(colored('Откуда берем?', color=user_color))
+#     qua = input(colored('Сколько берем?', color=user_color))
+#     step_successed = take_from_bunch(position=int(pos), quantity=int(qua))
+#     if step_successed:
+#         user_number = 2 if user_number == 1 else 1
+#     else:
+#         cprint('Невозможный ход!', color='red')
+#     if is_gameover():
+#         break
+#
+# cprint('Выйграл игрок номер {}'.format(user_number), color='red')
---
+++ python_snippets/probe.py
@@ -0,0 +1,9 @@
+x= 'gbvgbhb'
+def func_3():
+    global x
+    x = 'LOCO'
+    print('x in f1 =', x)
+
+print(x)
+func_3()
+print(x)
